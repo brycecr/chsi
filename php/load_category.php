93a7b1a67fc67adb6b53264efd1ea38a62da7067
Ruby Lee <rubylee@Rubys-MacBook-Pro.local>
2012-11-24 15:43:42 -0600

    Maps every attribute

--- php/load_category.php
+++ php/load_category.php
@@ -3,7 +3,7 @@
 
 	include 'configuration.php';
 	$category = $_GET['category'];
-	$query = sprintf("SELECT * FROM DataElementDescription WHERE PAGE_NAME = '%s'", mysql_real_escape_string($category));
+	$query = sprintf("SELECT * FROM DataElementDescription WHERE PAGE_NAME = '%s' AND COLUMN_NAME <> 'State_FIPS_Code' AND COLUMN_NAME <> 'County_FIPS_Code' AND COLUMN_NAME <> 'CHSI_County_Name' AND COLUMN_NAME <> 'CHSI_State_Name' AND COLUMN_NAME <> 'CHSI_State_Abbr' AND COLUMN_NAME <> 'Strata_ID_Number' AND COLUMN_NAME <> 'Strata_Determining_Factors'", mysql_real_escape_string($category));
  	$result = mysql_query($query);
   	$array = array();
   	while($r = mysql_fetch_assoc($result)) {
