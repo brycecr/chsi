2ba3c561ab189b3c9ddbd0adddd618b7a884778d
Ruby Lee <rubylee@rescomp-12-149360.stanford.edu>
2012-12-08 22:08:57 -0800

    map titles

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ function load_map(data, div_id) {
 	// This choropleth map code was seeded off an example by Mike Bostock
 	// using SVG data for backing map from Mike Bostock, Tom Carden, and
 	// the United States Census Bureau.
-	console.log(div_id);
+
 	$("#" + div_id).html('');
 	var path = d3.geo.path();
 
@@ -284,7 +284,7 @@ function load_attribute(attribute_div, category) {
 				map_data[("0" + data[i]['State_FIPS_Code'].toString()).slice(-2) + ("00" + data[i]['County_FIPS_Code'].toString()).slice(-3)] = parseInt(data[i][attribute_div.attr('id')]);
 			}
 
-			$("#map" + $("body").data('map_id') + "_title").text(attribute_div.attr('id'));
+			//$("#map" + $("body").data('map_id') + "_title").text(attribute_div.attr('id'));
 			load_map(map_data, 'map' + $("body").data('map_id'));
 			//update_scatterplot(map_data);
 			//load_parcoords(map_data);
