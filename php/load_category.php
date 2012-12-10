ade6d1cc08b7dce6a6c4537ba096202ebb0bc800
Ruby Lee <rubylee@rescomp-12-149360.stanford.edu>
2012-12-09 21:11:32 -0800

    pruning attributes

--- php/load_category.php
+++ php/load_category.php
@@ -9,7 +9,8 @@
 	'CHSI_County_Name' AND COLUMN_NAME <> 'CHSI_State_Name' AND
 	COLUMN_NAME <> 'CHSI_State_Abbr' AND COLUMN_NAME <>
 	'Strata_ID_Number' AND COLUMN_NAME <>
-	'Strata_Determining_Factors' AND COLUMN_NAME NOT LIKE '%Max_%' AND COLUMN_NAME NOT LIKE '%Min_%'";
+	'Strata_Determining_Factors' AND COLUMN_NAME NOT LIKE '%Max_%' AND COLUMN_NAME NOT LIKE '%Min_%' 
+	AND COLUMN_NAME NOT LIKE '%_Ind%'";
 	$result = $db -> query($query);
   	$array = array();
   	while($r = $result -> fetch()) {
