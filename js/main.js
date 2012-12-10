ae39cf6b517a30ffdf5a19b5aabc8e48d2960766
Ruby Lee <rubylee@rescomp-12-149360.stanford.edu>
2012-12-10 00:46:14 -0800

    adding feature to expand map

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 $(document).ready(init);
 
 function init() {
-	$("document").data('map_ids_present', new Object());	// tracks map ids with data (key: map id, value: true/false)
-	console.log(typeof $("document").data('map_ids_present'));
-	$("document").data('map_id_active', 1);					// set active map id to 1 (default)
+	$("document").data('map_ids_present', {});		// tracks map ids with data (key: map id, value: true/false)
+	$("document").data('map_id_active', 1);			// set active map id to 1 (default)
 	$("#map1").css('background', '#EFEFEF');
 
 	load_nav();
@@ -401,13 +400,14 @@ function load_attribute(attr_id, category) {
 
 			var map_id = $("document").data('map_id_active');
 
-			console.log(typeof $("document").data('map_ids_present'));
-			if (!(map_id in $("document").data('map_ids_present'))) {	// update map_ids_present
-				$("document").data("map_ids_present")[map_id] = true;
+			if (Object.keys($("document").data('map_ids_present')).length > 0)
+				if (!(map_id in $("document").data('map_ids_present'))) {	// update map_ids_present
+					$("document").data("map_ids_present")[map_id] = true;
+				}
 			}
 
-			$("document").data('map' + map_id + '_data', map_data);		// update map_i_data
-			$("document").data('map' + map_id + '_title', attr_id);		// update map_i_title
+			$("document").data('map' + map_id + '_data', map_data);			// update map_i_data
+			$("document").data('map' + map_id + '_title', attr_id);			// update map_i_title
 
 			$("#map" + map_id + "_title").html(attr_id + '<div class="map_title_option" id="map' + map_id + '_clear"><a href="javascript:void(0);">clear</a></div><div class="map_title_option" id="map' + map_id + '_expand"><a href="javascript:void(0);">expand</a></div>');
 			$("#map" + map_id + "_expand").click(function(map_id, attr_id) {
