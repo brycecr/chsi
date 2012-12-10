74eb77c5f03b8bbd001d048cdaac3d8cf88dcd2e
Ruby Lee <rubylee@rescomp-12-149360.stanford.edu>
2012-12-10 11:41:08 -0800

    adding scatterplots

--- js/main.js
+++ js/main.js
@@ -308,7 +308,7 @@ function load_scatterplots() {
 		    svg.append("text")
 		    .attr("class", "scatterplot_label")
 		    .attr("text-anchor", "end")
-		    .attr("y", 120)
+		    .attr("y", 50)
 		    .attr("dy", "10px")
 		    .attr("transform", "rotate(-90)")
 		    .text($("body").data('map' + map_ids[j].toString() + '_title'));
