035e41a666dbdd69e6912d222e2ebabba2de3317
Ruby Lee <rubylee@rescomp-12-149360.stanford.edu>
2012-12-09 21:16:57 -0800

    pruning attributes

--- php/load_category.php
+++ php/load_category.php
@@ -10,7 +10,7 @@
 	COLUMN_NAME <> 'CHSI_State_Abbr' AND COLUMN_NAME <>
 	'Strata_ID_Number' AND COLUMN_NAME <>
 	'Strata_Determining_Factors' AND COLUMN_NAME NOT LIKE '%Max_%' AND COLUMN_NAME NOT LIKE '%Min_%' 
-	AND COLUMN_NAME NOT LIKE '%_Ind%'";
+	AND (PAGE_NAME NOT LIKE 'RelativeHealthImportance' AND COLUMN_NAME NOT LIKE '%_Ind%')";
 	$result = $db -> query($query);
   	$array = array();
   	while($r = $result -> fetch()) {
